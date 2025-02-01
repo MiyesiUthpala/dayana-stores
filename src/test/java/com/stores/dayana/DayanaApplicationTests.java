07c5bbc9c9cd2ff7232f3a98d2ce72796578f240
Miyesi Welagedara <miyasiuthpala@gmail.com>
2025-02-01 12:59:54 +0530

    initial commit

---
+++ src/test/java/com/stores/dayana/DayanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stores.dayana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
