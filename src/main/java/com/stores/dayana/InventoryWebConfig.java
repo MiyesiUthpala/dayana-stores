e751a9eedbf874d66495b551fd148b1fd134f9d6
Michael-joel <michaeljes24@gmail.com>
2025-03-20 14:07:44 +0530

    renamed Webconfig file to InventoryWebConfig

--- src/main/java/com/stores/dayana/InventoryWebConfig.java
+++ src/main/java/com/stores/dayana/InventoryWebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class InventoryWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
